324a14d03e56df08e849af3db2e24a0d4b223c4b
gf091080 <gf091080@C39464.ens.uqam.ca>
2018-09-17 11:40:18 -0400

    2nd

--- src/bonjour/Bonjour.java
+++ src/bonjour/Bonjour.java
@@ -16,6 +16,8 @@ public class Bonjour {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+ args[0] + args [1]);
+         System.out.println("Bonjour ");
+        
     }
     
 }
